03b54aaea8069765916bbd66277b8a48cdba1447
Gerhardus <hardus.cronje@gmail.com>
2026-03-15 22:45:43 +0200

    feat: removed non channel based implementation

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,17 @@
-use std::sync::{Arc, Mutex};
-
 use cursive::{
     Cursive, CursiveExt,
     event::{Event, Key},
-    view::{Nameable, Resizable, ScrollStrategy, Scrollable, SizeConstraint},
-    views::{
-        Dialog, LinearLayout, NamedView, ResizedView, ScrollView, TextArea, TextContent, TextView,
-    },
+    view::Nameable,
+    views::{LinearLayout, TextContent},
 };
-use cursive_aligned_view::Alignable;
 use terminai::{
     custom_views::{llm_prompt_view::LlmPromptView, llm_response_view::LlmResponseView},
-    llm_context::{LlmContext, Message, Role},
-    models::ollama::{self, LlmContextManager},
+    llm_context::LlmContext,
 };
 
 fn main() {
     let mut siv = Cursive::new();
 
-    let sink = siv.cb_sink().clone();
-
-    let context = Arc::new(Mutex::new(LlmContext::new(sink, TextContent::new(""))));
-    let context1 = context.clone();
-
-    siv.add_layer(ResizedView::with_full_screen(
-        LinearLayout::vertical()
-            .child(ResizedView::new(
-                SizeConstraint::Full,
-                SizeConstraint::Free,
-                TextView::empty().scrollable().with_name("output-area"),
-            ))
-            .child(
-                ResizedView::with_full_width(
-                    Dialog::around(
-                        TextArea::new()
-                            .scrollable()
-                            .scroll_strategy(ScrollStrategy::StickToBottom)
-                            .with_name("prompt-area"),
-                    )
-                    .title("Prompt")
-                    .button("PROMPT", move |s| {
-                        let prompt = s.call_on_name(
-                            "prompt-area",
-                            |v: &mut NamedView<ScrollView<TextArea>>| {
-                                v.get_mut().get_inner_mut().get_content().to_owned()
-                            },
-                        );
-                        if let Some(prompt) = prompt {
-                            context
-                                .clone()
-                                .add_message(Message::new(Role::User, prompt));
-                            ollama::stream_res_to_gui(s, context.clone());
-                        }
-                    })
-                    .button("CLEAR INPUT", |s| {
-                        s.call_on_name("prompt-area", |v: &mut NamedView<ScrollView<TextArea>>| {
-                            v.get_mut().get_inner_mut().set_content("");
-                        });
-                    })
-                    .button("CLEAR OUTPUT", |s| {
-                        s.call_on_name("output-area", |v: &mut NamedView<ScrollView<TextView>>| {
-                            v.get_mut().get_inner_mut().set_content("");
-                        });
-                    })
-                    .button("CLEAR CONTEXT", move |_s| {
-                        context1.clear();
-                    }),
-                )
-                .fixed_height(10),
-            )
-            .with_name("prompt-container")
-            .align_bottom_center(),
-    ));
-
-    let main_screen_id = siv.active_screen();
-    let test_screen_id = siv.add_screen();
-
-    siv.set_screen(test_screen_id);
     let sink = siv.cb_sink().clone();
 
     let text_content = TextContent::new("");
@@ -92,6 +27,9 @@ fn main() {
             .child(LinearLayout::horizontal().with_name("loading-view"))
             .child(llm_prompt_view),
     );
+
+    let main_screen_id = siv.active_screen();
+    let test_screen_id = siv.add_screen();
 
     siv.add_global_callback(Event::Key(Key::F1), move |s| {
         s.set_screen(main_screen_id);
--- src/models/ollama.rs
+++ src/models/ollama.rs
@@ -1,141 +1,14 @@
 use anyhow::Result;
-use std::{
-    collections::HashMap,
-    io::Read,
-    sync::{Arc, Mutex, mpsc::Sender},
-    thread,
-    time::Duration,
-};
+use std::{collections::HashMap, io::Read, sync::mpsc::Sender};
 
-use cursive::{
-    CbSink, Cursive,
-    views::{DummyView, LinearLayout, NamedView, ScrollView, TextArea, TextView},
-};
+use cursive::CbSink;
 use reqwest::blocking::Response;
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    custom_views::spinner_view::SpinnerView,
-    llm_context::{LlmContext, LlmContextUpdateMessage, LoadingState, Message, Role},
+    llm_context::{LlmContextUpdateMessage, LoadingState, Message, Role},
     utils::show_message,
 };
-
-pub trait LlmContextManager {
-    fn add_message(self, msg: Message) -> Self;
-    fn clear(&self);
-}
-
-impl LlmContextManager for Arc<Mutex<LlmContext>> {
-    fn add_message(self, msg: Message) -> Self {
-        if let Ok(mut context) = self.lock() {
-            context.conversation.push(msg);
-        }
-        self
-    }
-
-    fn clear(&self) {
-        if let Ok(mut context) = self.lock() {
-            context.conversation.clear();
-        }
-    }
-}
-
-pub fn stream_res_to_gui(s: &mut Cursive, context: Arc<Mutex<LlmContext>>) {
-    let sink = s.cb_sink().clone();
-
-    thread::spawn(move || {
-        let sink2 = sink.clone();
-        // NOTE: ASYNC REQ STARTS
-        let _ = sink.send(Box::new(move |s| {
-            s.call_on_name("prompt-container", |v: &mut NamedView<LinearLayout>| {
-                v.get_mut().insert_child(
-                    1,
-                    LinearLayout::horizontal()
-                        .child(DummyView)
-                        .child(SpinnerView::new(sink2, Duration::from_millis(50)))
-                        .child(DummyView)
-                        .child(TextView::new("Thinking...")),
-                );
-            });
-            s.call_on_name("prompt-area", |v: &mut NamedView<ScrollView<TextArea>>| {
-                v.get_mut().get_inner_mut().set_content("");
-            });
-        }));
-
-        let res = call_api_with_mux(context.clone());
-
-        let mut res = match res {
-            Ok(res) => res,
-            Err(e) => {
-                let e = format!("{e}");
-                let _ = sink.send(Box::new(move |s| {
-                    // REMOVE LOADING SPINNER IF RES IS NOT OK.
-                    s.call_on_name("prompt-container", |v: &mut NamedView<LinearLayout>| {
-                        v.get_mut().remove_child(1);
-                    });
-                    show_message(s, e);
-                }));
-                return;
-            }
-        };
-
-        // NOTE: STREAMING STARTS
-        let _ = sink.send(Box::new(move |s| {
-            s.call_on_name("prompt-container", |v: &mut NamedView<LinearLayout>| {
-                v.get_mut().remove_child(1);
-            });
-        }));
-
-        let full_message = Arc::new(Mutex::new(String::new()));
-        let mut buf = [0; 0x1FF];
-        while let Ok(x) = res.read(&mut buf)
-            && x > 0
-        {
-            // Each iteration of the loop needs a reference.
-            let full_message = full_message.clone();
-            let _ = sink.send(Box::new(move |s| {
-                let sink = s.cb_sink().clone();
-                s.call_on_name("output-area", |v: &mut NamedView<ScrollView<TextView>>| {
-                    let res = String::from_utf8(buf[..x].to_vec()).expect("all valid utf8");
-
-                    let parsed: Result<OllamaStreamingResponse, serde_json::Error> =
-                        serde_json::from_str(&res);
-
-                    match parsed {
-                        Ok(parsed) => {
-                            if let Ok(mut part) = full_message.clone().lock() {
-                                part.extend(parsed.message.content.chars());
-                            }
-                            v.get_mut().get_inner_mut().append(parsed.message.content);
-                            v.get_mut().scroll_to_bottom();
-                        }
-                        Err(e) => {
-                            let _ = sink.send(Box::new(move |s| {
-                                if e.is_eof() {
-                                    return;
-                                }
-                                show_message(s, format!("Err: {}, Res: {:?}", e, res.clone()));
-                            }));
-                        }
-                    }
-                });
-            }));
-            buf.fill(0);
-        }
-
-        if let Ok(msg) = full_message.lock() {
-            context.add_message(Message::new(Role::System, msg.to_string()));
-        }
-
-        // Add new line after response
-        let _ = sink.send(Box::new(|s| {
-            s.call_on_name("output-area", |v: &mut NamedView<ScrollView<TextView>>| {
-                v.get_mut().get_inner_mut().append("\n\n");
-                v.get_mut().scroll_to_bottom();
-            });
-        }));
-    });
-}
 
 pub fn stream_res_to_llm_context(
     sender: Sender<LlmContextUpdateMessage>,
@@ -212,35 +85,6 @@ pub fn stream_res_to_llm_context(
         buf.fill(0);
     }
     let _ = sender.send(LlmContextUpdateMessage::CurrentMessageEnd(role));
-}
-
-fn call_api_with_mux(context: Arc<Mutex<LlmContext>>) -> Result<Response> {
-    let client = reqwest::blocking::Client::new();
-
-    let messages = match context.lock() {
-        Ok(context) => context
-            .conversation
-            .iter()
-            .map(|x| OutgoingMessage {
-                role: x.role.to_string(),
-                content: x.content.clone(),
-            })
-            .collect::<Vec<_>>(),
-        Err(_) => vec![],
-    };
-
-    let req_body = OllamaStreamingRequest {
-        model: "gemma3".to_owned(),
-        messages,
-    };
-
-    let res = client
-        .post("http://localhost:11434/api/chat")
-        .header("Content-Type", "application/json")
-        .json(&req_body)
-        .send()?;
-
-    Ok(res)
 }
 
 pub fn call_api(messages: Vec<OutgoingMessage>) -> Result<Response> {
